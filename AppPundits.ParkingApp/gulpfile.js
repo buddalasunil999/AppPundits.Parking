848b81ca4ea7af01526541a7f5165e765c079bcd
buddalasunil999 <buddalasunil999@gmail.com>
2015-08-09 20:34:40 +1200

    working gulp

--- AppPundits.ParkingApp/gulpfile.js
+++ AppPundits.ParkingApp/gulpfile.js
@@ -1,1 +1,48 @@
-﻿
+﻿/// <binding BeforeBuild='scripts' ProjectOpened='watch' />
+// include plug-ins
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var del = require('del');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+var config = {
+    //Include all js files but exclude any min.js files
+    src: ['app/**/*.js', '!app/**/*.min.js'],
+}
+
+// Synchronously delete the output file(s)
+gulp.task('clean', function () {
+    del.sync(['app/all.min.js'])
+});
+
+// Combine and minify all files from the app folder
+gulp.task('scripts', ['clean'], function () {
+
+    return gulp.src(config.src)
+      .pipe(uglify())
+      .pipe(concat('all.min.js'))
+      .pipe(gulp.dest('app/'));
+});
+
+gulp.task('watch', function () {
+    return gulp.watch(config.src, ['scripts']);
+});
+
+// Watch scss AND html files, doing different things with each.
+gulp.task('serve', ['scripts'], function () {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch(config.src, ['scripts']).on("change", browserSync.reload);
+});
+
+//Set a default tasks
+gulp.task('default', ['scripts'], function () { });
